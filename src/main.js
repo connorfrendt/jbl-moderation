883d3bd9309465a86c55c249c3aa8fd8d7077ed5
Connor Frendt <connor.frendt@pollinate.com>
2019-10-02 14:49:59 -0700

    wip

--- src/main.js
+++ src/main.js
@@ -4,5 +4,16 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.spectrumcustomizer.com/api/external/jbl/orders/under-review')
+      .then(res => (this.info = res))
+  },
   render: h => h(App),
 }).$mount('#app')
