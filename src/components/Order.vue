854bc07f31f6b9ca17c6dfd4d38458dd2e7d2cf5
Jack Stringer <jack.stringer@pollinate.com>
2019-10-02 15:56:23 -0700

    Added order component file

---
+++ src/components/Order.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id='order'>
+        <img v-bind:src="imageUrl">
+        <button @click="approve">Approve</button>
+        <button @click="deny">Deny</button>
+    </div>
+</template>
+
+<script>
+import axios from axios;
+
+export default {
+    name: "order",
+    props: {
+        recipeId: String,
+    },
+    data: function(){
+        return {
+            imageUrl: ''
+        }
+    },
+    methods: {
+        approve: function(){
+            //make GET request to approval route
+        },
+
+        deny: function(denial_text){
+            //make GET request to denial route 
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
