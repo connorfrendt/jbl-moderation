c2efeaf4952d327405ad903511ed7858548a6a22
Connor Frendt <connor.frendt@pollinate.com>
2019-10-03 13:38:58 -0700

    trying to fix errors and make Order appear in Orders

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div id="app">
-    <HelloWorld msg="JBL Moderation Tool"/>
+    <Orders msg="JBL Moderation Tool"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Orders from './components/Orders.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Orders
   }
 }
 </script>
 
 <style>
+body {
+  background-color: black;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="hello">
+    <p>{{ info }}</p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+  name: 'Orders',
+  props: {
+    info: String
+  },
+  created() {
+    axios
+      .get('https://api.spectrumcustomizer.com/api/external/jbl/orders/under-review')
+      .then(res => {
+        this.info = res.data
+        })
+  }
 }
 </script>
+
+<style scoped>
+p {
+  color: white;
+}
+</style>
